a20d1e60edc4801ae7557d8be29f16e64932c18b
Mark Thomas <markthethomas@gmail.com>
2016-09-08 12:13:16 -0700

    Update 2-4.js

--- ch2/2-4.js
+++ ch2/2-4.js
@@ -1,54 +1,72 @@
 // ...
-		var Post = React.createClass({
-		    propTypes: {
-		        user: React.PropTypes.string.isRequired, //#A
-		        content: React.PropTypes.string.isRequired, //#A
-		        postID: React.PropTypes.string.isRequired, //#A
-		    },
-		    render: function() {
-		        return (
-		            React.createElement('div', {
-		                    className: 'post'
-		                },
-		                React.createElement('h2', {
-		                        className: 'postAuthor',
-		                        postID: this.props.postID
-		                    }, //#B
-		                    this.props.user, //#B
-		                    React.createElement('span', {
-		                            className: 'postBody'
-		                        },
-		                        this.props.content //#B
-		                    )
-		                )
-		            )
-		        );
-		    }
-		});
+var mountTarget = document.getElementById('ourFirstApp');
+var Post = React.createClass({
+    propTypes: {
+        user: React.PropTypes.string.isRequired, //#A
+        content: React.PropTypes.string.isRequired, //#A
+        id: React.PropTypes.number.isRequired, //#A
+    },
+    render: function() {
+        return (
+            React.createElement('div', {
+                    className: 'post'
+                },
+                React.createElement('h2', {
+                        className: 'postAuthor',
+                        id: this.props.id
+                    }, //#B
+                    this.props.user, //#B
+                    React.createElement('span', {
+                            className: 'postBody'
+                        },
+                        this.props.content //#B
+                    )
+                ),
+                this.props.children
+            )
+        );
+    }
+});
 
-		var Comment = React.createClass({
-		    propTypes: {
-		        id: React.PropTypes.string.isRequired,
-		        content: React.PropTypes.string.isRequired,
-		        user: React.PropTypes.string.isRequired,
-		    },
-		    render: function() {
-		        return (
-		            React.createElement('div', {
-		                    className: 'comment'
-		                },
-		                React.createElement('h2', {
-		                        className: 'commentAuthor'
-		                    },
-		                    this.props.user, //#B
-		                    React.createElement('span', {
-		                            className: 'commentContent'
-		                        },
-		                        this.props.content //#B
-		                    )
-		                )
-		            )
-		        )
-		    }
-		});
+var Comment = React.createClass({
+    propTypes: {
+        id: React.PropTypes.number.isRequired,
+        content: React.PropTypes.string.isRequired,
+        user: React.PropTypes.string.isRequired,
+    },
+    render: function() {
+        return (
+            React.createElement('div', {
+                    className: 'comment'
+                },
+                React.createElement('h2', {
+                        className: 'commentAuthor'
+                    },
+                    this.props.user,
+                    React.createElement('span', {
+                            className: 'commentContent'
+                        },
+                        this.props.content
+                    )
+                )
+            )
+        );
+    }
+});
+
+var App = React.createElement(Post, {
+        id: 1,
+        content: " said: This is a post!",
+        user: 'mark',
+    },
+    React.createElement(Comment, {
+            id: 2,
+            user: 'bob',
+            content: " commented: wow! how cool!"
+        },
+        null
+    )
+);
+
+ReactDOM.render(App, mountTarget);
 // ...
